f4f14e2e41d325f1986a09ca1031ceeb19976e34
TommiON <44175014+TommiON@users.noreply.github.com>
2019-02-18 10:52:06 +0200

    Initial Commit

---
+++ PaineitaOn/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PaineitaOn
+//
+//  Created by Tommi Niittymies on 18.2.2019.
+//  Copyright © 2019 Tommi Niittymies. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PaineitaOn/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PaineitaOn
+//
+//  Created by Tommi Niittymies on 18.2.2019.
+//  Copyright © 2019 Tommi Niittymies. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
